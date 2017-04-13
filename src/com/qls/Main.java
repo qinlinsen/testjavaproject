ba1f1b16f2c39f2e3144b214ac1cab67dc17dccb
abrahamlincoln <abrahamlincoln@aliyun.com>
2017-04-13 21:11:40 +0800

    commit Test2 and modify Main

--- src/com/qls/Main.java
+++ src/com/qls/Main.java
@@ -3,6 +3,6 @@ package com.qls;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world,this is my first java programmer");
+        System.out.println("hello world,this is my fir");
     }
 }
---
+++ src/com/qls/Test2.java
@@ -0,0 +1,10 @@
+package com.qls;
+
+/**
+ * Created by Administrator on 2017/4/13.
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("h1");
+    }
+}
