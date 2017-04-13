65f33f018f1162ae8bb8641790a00a973e7b3b01
abrahamlincoln <abrahamlincoln@aliyun.com>
2017-04-13 12:35:18 +0800

    commit two simplest class

--- src/com/qls/Main.java
+++ src/com/qls/Main.java
@@ -3,6 +3,6 @@ package com.qls;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("hello world,this is my first java programmer");
     }
 }
---
+++ src/com/qls/Test.java
@@ -0,0 +1,12 @@
+package com.qls;
+
+/**
+ * Created by 秦林森 on 2017/4/13.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("ouyangfeng is the most beautiful woman in the world");
+        System.out.println("yuanRiver is the beautiful river");
+        System.out.println("long live chair man mao");
+    }
+}
